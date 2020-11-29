833bbdf89b08c794adbb1274629ea20ef337619a
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-30 02:52:01 +0300

    Created Mechanics

---
+++ src/Mechanics.java
@@ -0,0 +1,24 @@
+public class Mechanics extends Physics {
+
+    private String[] subject = {"Dynamics","Statics"};
+
+    @Override
+    public String[] getSubject() {
+        return subject;
+    }
+
+    @Override
+    public void setSubject(String[] subject) {
+        this.subject = subject;
+    }
+
+    public void showAdditionalKnowledges() {
+
+        for (String str : subject) {
+
+            System.out.println("Acquired additional knowledges are: " + str + " ");
+        }
+
+
+    }
+}
